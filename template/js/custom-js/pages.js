cd696d63186b406dccfea611eb8a069e4de7f352
matheusgnreis <matheus@e-com.club>
2023-04-07 18:01:08 -0300

    chore: better code

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -12,71 +12,15 @@ $('.tabs').click((e) => {
                 if (tab.classList) {
                 if (dataTab === league) {
                     tab.classList.add('active')
+                    document.querySelector(`.${dataTab}`).classList.remove('d-none')
+                    document.querySelector(`.${dataTab}`).classList.add('d-flex')
                 } else {
                     tab.classList.remove('active')
+                    document.querySelector(`.${dataTab}`).classList.add('d-none')
+                    document.querySelector(`.${dataTab}`).classList.remove('d-flex')
                 }
               }
             }
-        }
-        if (league === 'ligue-1') {
-            $('.ligue-1').show()
-            $('.la-liga').hide()
-            $('.premier-league').hide()
-            $('.bundesliga').hide()
-            $('.serie-a').hide()
-            $('.other-clubs').hide()
-            $('.international').hide()
-
-        } else if (league === 'premier-league') {
-            $('.ligue-1').hide()
-            $('.la-liga').hide()
-            $('.premier-league').show()
-            $('.bundesliga').hide()
-            $('.serie-a').hide()
-            $('.other-clubs').hide()
-            $('.international').hide()
-        } else if (league === 'bundesliga') {
-            $('.ligue-1').hide()
-            $('.la-liga').hide()
-            $('.premier-league').hide()
-            $('.bundesliga').show()
-            $('.serie-a').hide()
-            $('.other-clubs').hide()
-            $('.international').hide()
-        } else if (league === 'serie-a') {
-            $('.ligue-1').hide()
-            $('.la-liga').hide()
-            $('.premier-league').hide()
-            $('.bundesliga').hide()
-            $('.serie-a').show()
-            $('.other-clubs').hide()
-            $('.international').hide()
-
-        } else if (league === 'other-clubs') {
-            $('.ligue-1').hide()
-            $('.la-liga').hide()
-            $('.premier-league').hide()
-            $('.bundesliga').hide()
-            $('.serie-a').hide()
-            $('.other-clubs').show()
-            $('.international').hide()
-
-        } else if (league === 'international') {
-            $('.ligue-1').hide()
-            $('.la-liga').hide()
-            $('.premier-league').hide()
-            $('.bundesliga').hide()
-            $('.serie-a').hide()
-            $('.other-clubs').hide()
-            $('.international').show()
-        } else if (league === 'la-liga') {
-            $('.ligue-1').hide()
-            $('.la-liga').show()
-            $('.premier-league').hide()
-            $('.bundesliga').hide()
-            $('.serie-a').hide()
-            $('.other-clubs').hide()
-            $('.international').hide()
         }
     }
 })
@@ -85,81 +29,23 @@ $('#col-leagues .stamps').click((e) => {
     if (e.target && e.target.dataset && e.target.dataset.liga) {
         const league = e.target.dataset.liga
         const tabs = $('.tabs-container .tabs .tab')
-        console.log(tabs)
         for (tabO in tabs) {
             let tab = tabs[tabO]
             if (tab.childNodes) {
                 const dataTab = tab.childNodes[0] && tab.childNodes[0].dataset && tab.childNodes[0].dataset.tab
                 console.log(dataTab)
                 if (tab.classList) {
-                if (dataTab === league) {
-                    tab.classList.add('active')
-                } else {
-                    tab.classList.remove('active')
-                }
+                    if (dataTab === league) {
+                        tab.classList.add('active')
+                        document.querySelector(`.${dataTab}`).classList.remove('d-none')
+                        document.querySelector(`.${dataTab}`).classList.add('d-flex')
+                    } else {
+                        tab.classList.remove('active')
+                        document.querySelector(`.${dataTab}`).classList.add('d-none')
+                        document.querySelector(`.${dataTab}`).classList.remove('d-flex')
+                    }
               }
             }
-        }
-        console.log(league)
-        if (league === 'ligue-1') {
-            $('.ligue-1').show()
-            $('.la-liga').hide()
-            $('.premier-league').hide()
-            $('.bundesliga').hide()
-            $('.serie-a').hide()
-            $('.other-clubs').hide()
-            $('.international').hide()
-
-        } else if (league === 'premier-league') {
-            $('.ligue-1').hide()
-            $('.la-liga').hide()
-            $('.premier-league').show()
-            $('.bundesliga').hide()
-            $('.serie-a').hide()
-            $('.other-clubs').hide()
-            $('.international').hide()
-        } else if (league === 'bundesliga') {
-            $('.ligue-1').hide()
-            $('.la-liga').hide()
-            $('.premier-league').hide()
-            $('.bundesliga').show()
-            $('.serie-a').hide()
-            $('.other-clubs').hide()
-            $('.international').hide()
-        } else if (league === 'serie-a') {
-            $('.ligue-1').hide()
-            $('.la-liga').hide()
-            $('.premier-league').hide()
-            $('.bundesliga').hide()
-            $('.serie-a').show()
-            $('.other-clubs').hide()
-            $('.international').hide()
-
-        } else if (league === 'other-clubs') {
-            $('.ligue-1').hide()
-            $('.la-liga').hide()
-            $('.premier-league').hide()
-            $('.bundesliga').hide()
-            $('.serie-a').hide()
-            $('.other-clubs').show()
-            $('.international').hide()
-
-        } else if (league === 'international') {
-            $('.ligue-1').hide()
-            $('.la-liga').hide()
-            $('.premier-league').hide()
-            $('.bundesliga').hide()
-            $('.serie-a').hide()
-            $('.other-clubs').hide()
-            $('.international').show()
-        } else if (league === 'la-liga') {
-            $('.ligue-1').hide()
-            $('.la-liga').show()
-            $('.premier-league').hide()
-            $('.bundesliga').hide()
-            $('.serie-a').hide()
-            $('.other-clubs').hide()
-            $('.international').hide()
         }
     }
 });
